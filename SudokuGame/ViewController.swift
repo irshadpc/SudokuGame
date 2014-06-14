88be4c84b8b435a03810e56ce18f0cf42088a070
Alfred Jeremy Cepeda <AlfredJCepeda@gmail.com>
2014-06-13 19:17:21 -0700

    Initial Commit

---
+++ SudokuGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SudokuGame
+//
+//  Created by Alfred Cepeda on 6/13/14.
+//  Copyright (c) 2014 Alfred Cepeda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
